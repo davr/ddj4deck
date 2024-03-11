9d1e27212e2583fd3e1f982419b83b88f0e1ab58
davr <davr@davr.org>
2024-03-11 11:58:22 -0700

    Add more error handling to te.py

--- te.py
+++ te.py
@@ -1,6 +1,7 @@
 import ctypes
 from ctypes import *
 import time
+import win32api
 
 vm = ctypes.WinDLL("c:/windows/system32/teVirtualMIDI.dll")
 getver = vm.virtualMIDIGetVersion
@@ -35,6 +36,24 @@ buf1=buf2=None
 na=0
 nb=0
 
+import win32con
+
+def get_system_error_message(error_code=None):
+    try:
+        if error_code is None:
+            error_code = win32api.GetLastError()
+        # Format the error message
+        error_message = win32api.FormatMessage(
+            win32con.FORMAT_MESSAGE_FROM_SYSTEM,
+            None,
+            error_code,
+            0,
+            None,
+        )
+        return error_message.strip()  # Remove any leading/trailing spaces
+    except Exception as e:
+        return f"Error retrieving system message: {e}"
+
 def tester():
     global porta,portb
     def mycb(port, data, length, cbinst):
@@ -42,7 +61,10 @@ def tester():
 
     porta = createPort2("PIONEER DDJ-SX2", None, None, 10240, 1 + 2 + 4 + 8)
     portb = createPort2("Internal", None, None, 10240, 1 + 2 + 4 + 8)
-    print(porta,portb)
+    print("Port handles:",porta,portb)
+    if porta is None or portb is None:
+        print("Could not create ports? ERR:",win32api.GetLastError(), get_system_error_message())
+        return
     t=0
     def atob():
         global na,porta,portb
@@ -50,16 +72,26 @@ def tester():
         while True:
             sz = c_int(10240) 
             res = getData(porta, buf1, byref(sz))
-            sendData(portb, buf1, sz)
-            na+=1
+            if res:
+                sendData(portb, buf1, sz)
+                na+=1                
+            else:
+                print("ERR a->b:",win32api.GetLastError(), get_system_error_message())
+                time.sleep(0.1)
+                #break
     def btoa():
         global nb,porta,portb
         buf2 = create_string_buffer(10240)
         while True:
             sz = c_int(10240) 
             res = getData(portb, buf2, byref(sz))
-            sendData(porta, buf2, sz)
-            nb+=1
+            if res:
+                sendData(porta, buf2, sz)            
+                nb+=1
+            else:
+                print("ERR b->a:",win32api.GetLastError(), get_system_error_message())
+                time.sleep(0.1)
+                #break
     from threading import Thread
     threada = Thread(target=atob, daemon=True)
     threadb = Thread(target=btoa, daemon=True)
